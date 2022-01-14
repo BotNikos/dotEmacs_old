6b889550b7d1657c0889ead9075022aa438ca8af
Nikita Bolotov <33418579+BotNikos@users.noreply.github.com>
2022-01-14 23:44:32 +0300

    change emmet-mode key-binding

--- config.el
+++ config.el
@@ -1,56 +1,36 @@
-;; Some default emacs settings
 (setq user-full-name "Nikita Bolotov"
       user-mail-address "boss.nikitos1212@mail.ru")
 
-(setq doom-theme 'doom-monokai-pro)
+;; envirovment settings
+(set-frame-font "Fira Code 14" nil t)
 
-(set-frame-font "Monaco 15" nil t)
-(global-set-key [f2] 'neotree-toggle)
-
-(add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
-
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
+(setq-default indent-tabs-moe nil)
+(setq tab-width 4)
 (setq indent-line-function 'insert-tab)
 
-(setq tramp-default-method "ssh")
-(setq tramp-auto-save-directory "~/tramp-autosave")
+(defvaralias 'c-basic-offset 'tab-width)
+(defvaralias 'cperl-indent-level 'tab-width)
+
+(add-hook 'html-mode-hook
+          (lambda ()
+            (set (make-local-variable 'sgml-basic-offset) 4)))
+
+;; custom key-bindings
+(global-set-key (kbd "C-/") 'comment-line)
+(global-set-key (kbd "TAB") 'emmet-insert)
+
+;; cusom functions
+(defun emmet-insert ()
+  (interactive)
+  (setq current-position (point))
+  (setq char-before (buffer-substring (- current-position 1) current-position))
+  (if (or (equal char-before "\n") (equal char-before " "))
+      (insert "    ")
+    (emmet-expand-line nil)))
+
+(setq doom-theme 'doom-dracula)
+
 
 (setq org-directory "~/org/")
+
 (setq display-line-numbers-type t)
-
-
-;; My "FTP" to lycoris.su
-(defun uploadAllToLycoris()
-        "upload all files and directories to lycoris.su"
-        (interactive)
-        (shell-command "sh ~/uploadAll.sh")
-        (message "All uploads complete"))
-
-(defun uploadOneToLycoris()
-        "upload one directory to lycoris.su"
-        (interactive)
-        (setq directory (read-string "Enter directory name for uploading: "))
-        (shell-command (format "sh ~/uploadOne.sh %s" directory))
-        (message "%s directory is uploaded" directory))
-
-
-(defun downloadAllFromLycoris()
-        "donwload all files and directories from lycoris.su"
-        (interactive)
-        (shell-command "sh ~/downloadAll.sh")
-        (message "All downloads complete"))
-
-
-(defun downloadOneFromLycoris()
-        "download one directory from lycoris.su"
-        (interactive)
-        (setq directory (read-string "Enter directory name for downloading: "))
-        (shell-command (format "sh ~/downloadOne.sh %s" directory))
-        (message "%s directory is downloaded" directory))
-
-
-(global-set-key (kbd "M-<f1>") 'uploadAllToLycoris)
-(global-set-key (kbd "M-<f2>") 'uploadOneToLycoris)
-(global-set-key (kbd "C-<f1>") 'downloadAllFromLycoris)
-(global-set-key (kbd "C-<f2>") 'downloadOneFromLycoris)
